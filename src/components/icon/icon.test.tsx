fa8efde68e3ad5b58aeb9f29518444f6c1c6be83
MateuszPalka <mateuszpalka90@gmail.com>
2022-10-16 15:33:29 +0200

    add test

---
+++ src/components/icon/icon.test.tsx
@@ -0,0 +1,25 @@
+import {
+  findByText,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { Icon, IconName } from "./icon";
+import { icons } from "./icons";
+import { axe, toHaveNoViolations } from "jest-axe";
+import React from "react";
+
+describe("<Icon />", () => {
+  expect.extend(toHaveNoViolations);
+  it.each(Object.keys(icons) as IconName[])(
+    "renders correct %s icon without violations",
+    async (iconName: IconName) => {
+      const { container } = render(<Icon icon={iconName} />);
+      await screen.findByText(/svg/i);
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+      expect(screen.getByRole("img")).toHaveAttribute("aria-label", iconName);
+    }
+  );
+});
--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -24,7 +24,8 @@ export const Icon = ({ icon, className, rotate, color, ...rest }: Props) => {
   return (
     <div
       className={classnames("flex items-center justify-center", className)}
-      role="presentation"
+      aria-label={icon}
+      role="img"
       style={{
         transform: rotate ? `rotate(${rotate}deg)` : undefined,
       }}
