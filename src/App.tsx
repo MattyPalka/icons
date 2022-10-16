151c85a587580135b0aa83ac7c143c7d2e0f923b
MateuszPalka <mateuszpalka90@gmail.com>
2022-10-16 12:14:57 +0200

    add missing key

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="w-full h-full">
       <div className="grid gap-4 p-4 grid-cols-[repeat(auto-fill,minmax(170px,_1fr))]">
         {Object.keys(icons).map((icon) => (
-          <div className="flex flex-col items-center">
+          <div key={icon} className="flex flex-col items-center">
             <Icon icon={icon as IconName} />
             <span>{icon}</span>
           </div>
